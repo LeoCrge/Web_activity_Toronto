afed52229e99e56c434bb6df3dafcf1b400054b5
BoumedineRayan <151127114+BoumedineRayan@users.noreply.github.com>
2025-11-04 21:19:35 +0100

    last push

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static files like CSS, JS, and images
+
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
